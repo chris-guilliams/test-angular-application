619d6472e8e1ce93d03644d2b72e8ddb5c0b1ab7
Christopher Guilliams <chris.guilliams@modea.com>
2022-04-18 17:23:11 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'css-specificity';
+
+  events$: Observable<any>
+  constructor(private http: HttpClient) {
+    this.events$ = this.http.get('http://spartanburgstg.prod.acquia-sites.com/api/events/list')
+
+  }
+  ngOnInit(): void {
+  }
+
+
 }
